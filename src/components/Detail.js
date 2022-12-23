a0cda61cd08aba486420bad69cfe25a96ff754ba
yudish <yudishchakrawarty3042@gmail.com>
2022-12-23 23:56:14 +0530

    Detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,7 @@
 import React from 'react'
-
-const Detail = () => {
+import { Typography,Box,Stack } from '@mui/material'
+const Detail = ({exerciseDetail}) => {
+  const {bodyPart,gifUrl,name,target,equipment}= exerciseDetail;
   return (
     <div>
       
