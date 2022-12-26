f81badeb7ce72e74288f3566b09c1fff7b645126
yudish <yudishchakrawarty3042@gmail.com>
2022-12-26 19:32:11 +0530

    details

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -6,13 +6,13 @@ const biceps = '../567521.png';
 const Detail = ({exerciseDetail}) => {
   const {bodyPart,gifUrl,name,target,equipment}= exerciseDetail;
   return (
-    <Stack gap={"60px"} sx={{flexDirection:{lg:'row'},p:' 20px',alignItems:'center'}}>
+    <Stack gap={"60px"} sx={{flexDirection:{lg:'row'},p:' 20px',alignItems:'center',ml:"-35rem",mt:"3rem"}}>
       <img src={gifUrl} alt="name" loading="lazy"/> 
-      <Stack sx={{gap:{lg:'35px',xs:'220px'},width:"20%"}}>
-        <Typography variant='h3'>
+      <Stack sx={{gap:{lg:'35px',xs:'220px'},flexBasis:"20%"}}>
+        <Typography width={"600px"} variant='h3'>
           {name}
         </Typography>
-        <Typography>
+        <Typography variant='h6' width={"600px"}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta repudiandae ipsam voluptates nulla, iure expedita fuga distinctio aut nisi architecto quibusdam rerum sunt modi numquam consectetur eos consequuntur, praesentium iste. Quidem quis odit ipsam, dolorem eos aspernatur minus tempore ratione nesciunt eius nostrum, atque aliquid numquam, consequuntur rerum obcaecati cumque.
         </Typography>
       </Stack>
