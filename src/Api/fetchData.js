dcb10bda4309b4e4572df5f7a2be01549f3c8f9a
yudish <yudishchakrawarty3042@gmail.com>
2022-12-26 18:53:38 +0530

    ex-details

--- src/Api/fetchData.js
+++ src/Api/fetchData.js
@@ -1,8 +1,7 @@
 export const ExerciseOptions = {
   method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    'X-RapidAPI-Key': '3382780f71msh688864f47768886p1c9810jsn06a2d87b739f',
+    'X-RapidAPI-Key': '10e96219acmsh9d33565e1e09d0dp1d5ed7jsn82dd488e67aa',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -10,13 +9,19 @@ export const ExerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
-  },
+    'X-RapidAPI-Key': '10e96219acmsh9d33565e1e09d0dp1d5ed7jsn82dd488e67aa',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
 };
 
+
 export const fetchData = async(url,options)=>{
+  try {
     const response = await fetch(url,options);
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.log(error)
+  }
+    
 }
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Typography,Box,Stack } from '@mui/material'
+const absImg = '../abs-vector-1.jpg';
+const dumbells = '../567521.png';
+const biceps = '../567521.png'; 
 const Detail = ({exerciseDetail}) => {
   const {bodyPart,gifUrl,name,target,equipment}= exerciseDetail;
   return (
-    <div>
-      
-    </div>
+    <Stack gap={"60px"} sx={{flexDirection:{lg:'row'},p:' 20px',alignItems:'center'}}>
+      <img src={gifUrl} alt="name" loading="lazy"/> 
+      <Stack sx={{gap:{lg:'35px',xs:'220px'},width:"20%"}}>
+        <Typography variant='h3'>
+          {name}
+        </Typography>
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta repudiandae ipsam voluptates nulla, iure expedita fuga distinctio aut nisi architecto quibusdam rerum sunt modi numquam consectetur eos consequuntur, praesentium iste. Quidem quis odit ipsam, dolorem eos aspernatur minus tempore ratione nesciunt eius nostrum, atque aliquid numquam, consequuntur rerum obcaecati cumque.
+        </Typography>
+      </Stack>
+    </Stack>
   )
 }
 
